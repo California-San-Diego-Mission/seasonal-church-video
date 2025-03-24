2cafff0b8069667d787038a957525541313fa56c
advojote <joseph.h.davis@missionary.org>
2025-03-23 20:58:51 -0700

    back to iframe, screen is blank when you try to use video element, need to embed a webpage

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 window.onload = (event) => {
-    let videoElement = document.getElementById("mainVideo");
+    let iframe = document.getElementById("mainVideo");
     let viewportWidth = window.innerWidth;//example, 100
     let viewportHeight = window.innerHeight;//example, 100
-    let videoInitWidth = videoElement.offsetWidth;//example, 50
-    let videoInitHeight = videoElement.offsetHeight;//example, 20
+    let videoInitWidth = iframe.offsetWidth;//example, 50
+    let videoInitHeight = iframe.offsetHeight;//example, 20
     let widthRatio = videoInitWidth/viewportWidth;//example, 50/100 = 0.5
     let heightRatio = videoInitHeight/viewportHeight;//example, 20/100 = 0.2
 
@@ -15,7 +15,7 @@ window.onload = (event) => {
         scaleMultiplier = 1 / heightRatio;
     }
 
-    videoElement.width = videoInitWidth * scaleMultiplier;
-    videoElement.height = videoInitHeight * scaleMultiplier;
+    iframe.width = videoInitWidth * scaleMultiplier;
+    iframe.height = videoInitHeight * scaleMultiplier;
     alert("scale Multiplication done by " + scaleMultiplier + ".");
   };
