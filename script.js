cc9db327c5e410e2766d2404f8ce9a9fd4138cda
advojote <joseph.h.davis@missionary.org>
2025-03-23 20:48:38 -0700

    video insertion worked but stayed small, so I finished implementing a draft of the scale multiplier script

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-// window.onload = (event) => {
-//     iframe = document.getElementById("mainVideo");
-//     let viewportWidth = window.innerWidth;//example, 100
-//     let viewportHeight = window.innerHeight;//example, 100
-//     let videoInitWidth = iframe.offsetWidth;//example, 50
-//     let videoInitHeight = iframe.offsetHeight;//example, 20
-//     let widthRatio = videoInitWidth/viewportWidth;//example, 50/100 = 0.5
-//     let heightRatio = videoInitHeight/viewportHeight;//example, 20/100 = 0.2
+window.onload = (event) => {
+    iframe = document.getElementById("mainVideo");
+    let viewportWidth = window.innerWidth;//example, 100
+    let viewportHeight = window.innerHeight;//example, 100
+    let videoInitWidth = iframe.offsetWidth;//example, 50
+    let videoInitHeight = iframe.offsetHeight;//example, 20
+    let widthRatio = videoInitWidth/viewportWidth;//example, 50/100 = 0.5
+    let heightRatio = videoInitHeight/viewportHeight;//example, 20/100 = 0.2
 
-//     let scaleMultiplier = 1;
+    let scaleMultiplier = 1;
 
-//     if (widthRatio > heightRatio) { //if the fraction of the width that the video takes up is greater than the fraction of the height it takes up, the inverse fraction of the width ratio should be the width multiplier
-//         scaleMultiplier = 1 / widthRatio;
-//     } else { //otherwise, the inverse fraction of the height of the video will be the height multiplier
-//         scaleMultiplier = 1 / heightRatio;
-//     }
+    if (widthRatio > heightRatio) { //if the fraction of the width that the video takes up is greater than the fraction of the height it takes up, the inverse fraction of the width ratio should be the width multiplier
+        scaleMultiplier = 1 / widthRatio;
+    } else { //otherwise, the inverse fraction of the height of the video will be the height multiplier
+        scaleMultiplier = 1 / heightRatio;
+    }
 
+    iframe.width = videoInitWidth * scaleMultiplier;
+    iframe.height = videoInitHeight * scaleMultiplier;
 
-
-//   };
+  };
