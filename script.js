e2ee1ce3f81f9f19665ff3864b59790b2ec80bcd
advojote <joseph.h.davis@missionary.org>
2025-03-24 21:14:03 -0700

    ctrl+u on the site is not showing any changes in the js file, so I am putting the js in the html directly

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
- // Wait for the iframe to load and then initialize the player
- window.onload = function() {
-  alert("script working");
-  var iframe = document.getElementById('myIframe');
-  var player;
+//  // Wait for the iframe to load and then initialize the player
+//  window.onload = function() {
+//   alert("script working");
+//   var iframe = document.getElementById('myIframe');
+//   var player;
 
-  // Check if the iframe is properly loaded
-  iframe.onload = function() {
-    alert("iframe loaded");
-    // Initialize the Brightcove player using the iframe's source
-    player = videojs(iframe);
+//   // Check if the iframe is properly loaded
+//   iframe.onload = function() {
+//     alert("iframe loaded");
+//     // Initialize the Brightcove player using the iframe's source
+//     player = videojs(iframe);
 
-    // Listen for the 'ended' event (video completion)
-    player.on('ended', function() {
-      alert("The video has finished playing.");
-    });
-  };
-};
+//     // Listen for the 'ended' event (video completion)
+//     player.on('ended', function() {
+//       alert("The video has finished playing.");
+//     });
+//   };
+// };
