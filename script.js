1e143d7238f381945a884d93d9fe6210878e7500
advojote <joseph.h.davis@missionary.org>
2025-03-24 21:09:57 -0700

    the container and z index were the problem, so the play button works now but autoplay doesn't. added alerts to the js to see if the event listeners are working

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
  // Wait for the iframe to load and then initialize the player
  window.onload = function() {
+  alert("script working");
   var iframe = document.getElementById('myIframe');
   var player;
 
   // Check if the iframe is properly loaded
   iframe.onload = function() {
+    alert("iframe loaded");
     // Initialize the Brightcove player using the iframe's source
     player = videojs(iframe);
 
     // Listen for the 'ended' event (video completion)
     player.on('ended', function() {
-      console.log("Video has finished playing.");
       alert("The video has finished playing.");
     });
   };
