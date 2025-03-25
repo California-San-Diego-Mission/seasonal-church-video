8c4afdbf6642bc1cc90d2e0e82b6f55d5dd52854
advojote <joseph.h.davis@missionary.org>
2025-03-24 21:00:37 -0700

    add js alert for end of video, tell it to autoplay in video url

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
-// window.onload = (event) => {
-//     let iframe = document.getElementById("mainVideo");
-//     // let viewportWidth = window.innerWidth;//example, 100
-//     // let viewportHeight = window.innerHeight;//example, 100
-//     // let videoInitWidth = iframe.offsetWidth;//example, 50
-//     // let videoInitHeight = iframe.offsetHeight;//example, 20
-//     // let widthRatio = videoInitWidth/viewportWidth;//example, 50/100 = 0.5
-//     // let heightRatio = videoInitHeight/viewportHeight;//example, 20/100 = 0.2
+ // Wait for the iframe to load and then initialize the player
+ window.onload = function() {
+  var iframe = document.getElementById('myIframe');
+  var player;
 
-//     // let scaleMultiplier = 1;
+  // Check if the iframe is properly loaded
+  iframe.onload = function() {
+    // Initialize the Brightcove player using the iframe's source
+    player = videojs(iframe);
 
-//     // if (widthRatio > heightRatio) { //if the fraction of the width that the video takes up is greater than the fraction of the height it takes up, the inverse fraction of the width ratio should be the width multiplier
-//     //     scaleMultiplier = 1 / widthRatio;
-//     // } else { //otherwise, the inverse fraction of the height of the video will be the height multiplier
-//     //     scaleMultiplier = 1 / heightRatio;
-//     // }
-
-//     // iframe.width = videoInitWidth * scaleMultiplier;
-//     // iframe.height = videoInitHeight * scaleMultiplier;
-//     iframe.style.width = window.innerWidth;
-//     iframe.style.height = window.innerHeight;
-//     alert("scale Multiplication done by " + scaleMultiplier + ".");
-//   };
+    // Listen for the 'ended' event (video completion)
+    player.on('ended', function() {
+      console.log("Video has finished playing.");
+      alert("The video has finished playing.");
+    });
+  };
+};
